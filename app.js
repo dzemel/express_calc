9d0bb379cef9a4023bca15a3fb82ccefe6015044
David Zemel <zemel96@gmail.com>
2021-03-24 14:40:41 -0400

    mean route done

--- app.js
+++ app.js
@@ -2,21 +2,25 @@
 
 const express = require("express");
 const app = express();
+const { convertStrNums } = require("./utils");
+const { findMean, findMode, findMedian } = require("./stats");
 
 // useful error class to throw
 const { NotFoundError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
-
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
 
-
+app.get("/mean", function (req, res, next) {
+  let numbers = req.query.nums.split(",");
+  numbers = convertStrNums(numbers);
+  console.log(numbers);
+  return res.json({ operation: "mean", value: findMean(numbers) });
+});
 /** Finds median of nums in qs: returns {operation: "median", result } */
 
-
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
-
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
@@ -30,7 +34,5 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
   return res.status(status).json({ error: { message, status } });
 });
-
-
 
 module.exports = app;
--- utils.js
+++ utils.js
@@ -1,12 +1,16 @@
 const { BadRequestError } = require("./expressError");
 
-
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+  let numArr = [];
+  for (let element of strNums) {
+    element = +element;
+    numArr.push(element);
+  }
+  return numArr;
 }
-
 
 module.exports = { convertStrNums };
